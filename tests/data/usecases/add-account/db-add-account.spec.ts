bf5ff27d0294d768cf6b6726ed11548141b081ff
William Simionatto <william.sn0226@gmail.com>
2022-10-12 18:03:40 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- tests/data/usecases/add-account/db-add-account.spec.ts
+++ tests/data/usecases/add-account/db-add-account.spec.ts
@@ -94,4 +94,18 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
+
+  test('Should throw if AddAccountRepository thorws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+
+    const account = sut.add(accountData)
+    await expect(account).rejects.toThrow()
+  })
 })
