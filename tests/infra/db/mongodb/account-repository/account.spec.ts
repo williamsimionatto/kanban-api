d054595e709882291bc545fe0f12ea9ee596881c
williamsimionatto <william.sn0226@gmail.com>
2022-10-13 13:20:31 -0300

    refactor: create mapping to MongoHelper

--- tests/infra/db/mongodb/account-repository/account.spec.ts
+++ tests/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,7 +11,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    const accountCollection = MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
