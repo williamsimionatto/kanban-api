a2053959d72f82b44ded103bfc7bc50c7a1c754f
William Simionatto <william.sn0226@gmail.com>
2022-10-15 19:03:50 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- tests/validators/required-field-validation.spec.ts
+++ tests/validators/required-field-validation.spec.ts
@@ -9,4 +9,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
 })
