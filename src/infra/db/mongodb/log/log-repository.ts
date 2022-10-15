98039555404761e6d9e4268f3f84543335c5774c
William Simionatto <william.sn0226@gmail.com>
2022-10-15 10:19:15 -0300

    feat: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log/log-repository.ts
@@ -0,0 +1,9 @@
+import { LogErrorRepository } from '../../../../data/protocols/db/log'
+import { MongoHelper } from '../helpers'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    const errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.insertOne({ stack, date: new Date() })
+  }
+}
