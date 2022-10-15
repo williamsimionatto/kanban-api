bf451b2294c4e46d7904077d4ce4e45deba1b2e9
William Simionatto <william.sn0226@gmail.com>
2022-10-15 18:02:26 -0300

    test: ensure SignUpController returns 400 if Validation fails

--- tests/presentation/controllers/signup/signup-controller.spec.ts
+++ tests/presentation/controllers/signup/signup-controller.spec.ts
@@ -2,6 +2,7 @@ import { AddAccountRepository } from '../../../../src/data/protocols/db/account'
 import { AddAccount } from '../../../../src/domain/usecases'
 import { SignUpController } from '../../../../src/presentation/controllers/signup'
 import { InvalidParamError, MissingParamError, ServerError } from '../../../../src/presentation/errors'
+import { badRequest } from '../../../../src/presentation/helpers/http-helper'
 import { HttpRequest, Validation } from '../../../../src/presentation/protocols'
 import { EmailValidator } from '../../../../src/validation/protocols'
 
@@ -262,5 +263,12 @@ describe('SignUp Controller', () => {
     const httpRequest = makeHttpRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeHttpRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
