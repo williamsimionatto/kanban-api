687f4db7e55896942f63907d2393f2451d91d0c4
William Simionatto <william.sn0226@gmail.com>
2022-10-16 13:45:00 -0300

    test: ensure AddProjectController calls  AddProject with correct values

--- tests/presentation/controllers/project/add-project/add-project-controller.spec.ts
+++ tests/presentation/controllers/project/add-project/add-project-controller.spec.ts
@@ -1,14 +1,18 @@
+import { AddProject } from '../../../../../src/domain/usecases'
 import { AddProjectController } from '../../../../../src/presentation/controllers/project'
 import { badRequest } from '../../../../../src/presentation/helpers/http-helper'
 import { HttpRequest, Validation } from '../../../../../src/presentation/protocols'
+
+import faker from 'faker'
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     name: 'any_name',
     description: 'any_description',
     status: 'any_status',
-    startDate: 'any_startDate',
-    endDate: 'any_endDate'
+    startDate: faker.date.recent(),
+    endDate: faker.date.future()
   }
 })
 
@@ -21,21 +25,42 @@ const makeValidationStub = (): Validation => {
   return new ValidationStub()
 }
 
+const makeAddProjectStub = (): AddProject => {
+  class AddProjectStub implements AddProject {
+    async add (project: AddProject.Params): Promise<AddProject.Result> {
+      return new Promise(resolve => resolve(true))
+    }
+  }
+
+  return new AddProjectStub()
+}
+
 type SutTypes = {
   sut: AddProjectController
   validationStub: Validation
+  addProjectStub: AddProject
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = makeValidationStub()
-  const sut = new AddProjectController(validationStub)
+  const addProjectStub = makeAddProjectStub()
+  const sut = new AddProjectController(validationStub, addProjectStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    addProjectStub
   }
 }
 
 describe('AddProject Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('Should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
@@ -49,5 +74,13 @@ describe('AddProject Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+
+  test('Should call AddProject with correct values', async () => {
+    const { sut, addProjectStub } = makeSut()
+    const addSpy = jest.spyOn(addProjectStub, 'add')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
