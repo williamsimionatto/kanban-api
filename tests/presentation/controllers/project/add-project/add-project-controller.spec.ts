6c8ce924c0c317364828078f68ba53ad8d33e19c
William Simionatto <william.sn0226@gmail.com>
2022-10-16 13:24:16 -0300

    test: ensure AddProjectController return 400 if Validation returns an error

--- tests/presentation/controllers/project/add-project/add-project-controller.spec.ts
+++ tests/presentation/controllers/project/add-project/add-project-controller.spec.ts
@@ -1,4 +1,5 @@
 import { AddProjectController } from '../../../../../src/presentation/controllers/project'
+import { badRequest } from '../../../../../src/presentation/helpers/http-helper'
 import { HttpRequest, Validation } from '../../../../../src/presentation/protocols'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -41,5 +42,12 @@ describe('AddProject Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
