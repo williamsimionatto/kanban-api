e0f8e41c2400af2c793df605e0136f89d26fc4bf
William Simionatto <william.sn0226@gmail.com>
2022-10-15 21:50:11 -0300

    test: ensure BcryptAdapter throw if bcrypt compare throws

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -64,5 +64,14 @@ describe('Bcrypt Adapter', () => {
       const isValid = await sut.compare('any_value', 'any_hash')
       expect(isValid).toBe(false)
     })
+
+    test('Should throw if bcrypt compare throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
