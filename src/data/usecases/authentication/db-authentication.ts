3fc839f173a683e9c3dc224ccecadd1fa24dcff1
William Simionatto <william.sn0226@gmail.com>
2022-10-15 21:18:27 -0300

    feat: ensure DbAuthentication calls Encrypter with correct plaintext

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,17 +1,21 @@
 import { Authentication } from '../../../domain/usecases'
-import { HashComparer } from '../../protocols/cryptography'
+import { Encrypter, HashComparer } from '../../protocols/cryptography'
 import { LoadAccountByEmailRepository } from '../../protocols/db/account'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
   ) {}
 
   async auth (authentication: Authentication.Params): Promise<string> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
     if (account) {
-      await this.hashComparer.compare(authentication.password, account.password)
+      const isvalid = await this.hashComparer.compare(authentication.password, account.password)
+      if (isvalid) {
+        await this.encrypter.encrypt(account.id)
+      }
     }
 
     return null
