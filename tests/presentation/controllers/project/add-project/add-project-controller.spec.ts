5a557060eeaddb2d8ce78307289461c1c529ea1a
William Simionatto <william.sn0226@gmail.com>
2022-10-16 13:50:31 -0300

    test: ensure AddProjectController returns 204 on success

--- tests/presentation/controllers/project/add-project/add-project-controller.spec.ts
+++ tests/presentation/controllers/project/add-project/add-project-controller.spec.ts
@@ -1,6 +1,6 @@
 import { AddProject } from '../../../../../src/domain/usecases'
 import { AddProjectController } from '../../../../../src/presentation/controllers/project'
-import { badRequest, serverError } from '../../../../../src/presentation/helpers/http-helper'
+import { badRequest, noContent, serverError } from '../../../../../src/presentation/helpers/http-helper'
 import { HttpRequest, Validation } from '../../../../../src/presentation/protocols'
 
 import faker from 'faker'
@@ -91,5 +91,11 @@ describe('AddProject Controller', () => {
     })
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
