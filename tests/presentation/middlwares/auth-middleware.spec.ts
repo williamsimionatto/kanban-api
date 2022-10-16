b88cb2de23d65cfb4336513a9c820b0b1a308cfd
William Simionatto <william.sn0226@gmail.com>
2022-10-16 20:21:32 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct accessToken

--- tests/presentation/middlwares/auth-middleware.spec.ts
+++ tests/presentation/middlwares/auth-middleware.spec.ts
@@ -1,15 +1,51 @@
+import { LoadAccountByToken } from '../../../src/domain/usecases'
 import { AccessDeniedError } from '../../../src/presentation/errors'
 import { forbidden } from '../../../src/presentation/helpers/http-helper'
 import { AuthMiddleware } from '../../../src/presentation/middlewares'
 
-const makeSut = (): AuthMiddleware => {
-  return new AuthMiddleware()
+import faker from 'faker'
+
+const makeLoadAccountByTokenStub = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
+      return new Promise(resolve => resolve({
+        id: faker.datatype.uuid()
+      }))
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+
+type SutTypes = {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  return {
+    sut,
+    loadAccountByTokenStub
+  }
 }
 
 describe('Auth Middlware', () => {
   test('Should return 403 if x-access-token no exists in headers', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
