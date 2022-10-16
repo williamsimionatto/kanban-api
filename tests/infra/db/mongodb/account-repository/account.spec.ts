6a500a5a13b246e5edd337bcb5b0fd1b52ff41da
William Simionatto <william.sn0226@gmail.com>
2022-10-15 22:12:53 -0300

    test: ensure AccountMongoRepository return null if loadByEmail fails

--- tests/infra/db/mongodb/account-repository/account.spec.ts
+++ tests/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,12 @@
+import { AddAccountRepository } from '../../../../../src/data/protocols/db/account'
 import { AccountMongoRepository } from '../../../../../src/infra/db/mongodb'
 import { MongoHelper } from '../../../../../src/infra/db/mongodb/helpers'
+
+const makeFakeAccountData = (): AddAccountRepository.Params => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository()
@@ -19,14 +26,31 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return an account on success', async () => {
-    const sut = makeSut()
-    const isValid = await sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  describe('add()', () => {
+    test('Should return an account on add success', async () => {
+      const sut = makeSut()
+      const isValid = await sut.add(makeFakeAccountData())
+
+      expect(isValid).toBe(true)
+    })
+  })
+
+  describe('loadByEmail()', () => {
+    test('Should return an account on loadByEmail success', async () => {
+      const sut = makeSut()
+      await sut.add(makeFakeAccountData())
+      const account = await sut.loadByEmail('any_email@mail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
 
-    expect(isValid).toBe(true)
+    test('Should return null if loadByEmail fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByEmail('any_mail@mail.com')
+      expect(account).toBeFalsy()
+    })
   })
 })
