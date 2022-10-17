1e6220a578f2c6699f47bd29985648112a56e4d1
William Simionatto <william.sn0226@gmail.com>
2022-10-16 21:12:06 -0300

    feat: implements Dcrypter with tests

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -1,10 +1,14 @@
-import { Encrypter } from '../../data/protocols/cryptography'
+import { Decrypter, Encrypter } from '../../data/protocols/cryptography'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (plainText: string): Promise<string> {
     return jwt.sign({ id: plainText }, this.secret)
+  }
+
+  async decrypt (cipherText: string): Promise<string> {
+    return jwt.verify(cipherText, this.secret) as string
   }
 }
