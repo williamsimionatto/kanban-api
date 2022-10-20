68f60f8cc0379979ee0f10ac76fa333be27ac4da
williamsimionatto <william.sn0226@gmail.com>
2022-10-20 16:14:00 -0300

    feat: ensure project route emits corrects events on request

---
+++ src/main/factories/usecases/load-account/db-load-account-by-token-factory.ts
@@ -0,0 +1,11 @@
+import { DbLoadAccountByToken } from '../../../../data/usecases/load-account'
+import { LoadAccountByToken } from '../../../../domain/usecases'
+import { JwtAdapter } from '../../../../infra/cryptography'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb'
+import env from '../../../config/env'
+
+export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(jwtAdapter, accountMongoRepository)
+}
