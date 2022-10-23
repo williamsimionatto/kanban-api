c710faa338b3bb288b727180cbc145c4185f396b
William Simionatto <william.sn0226@gmail.com>
2022-10-23 09:15:42 -0300

    test: ensure AddOrganization return 204 on success

--- tests/presentation/controllers/organization/add-organization.spec.ts
+++ tests/presentation/controllers/organization/add-organization.spec.ts
@@ -1,7 +1,7 @@
 import faker from 'faker'
 import { AddOrganization } from '../../../../src/domain/usecases'
 import { AddOrganizationController } from '../../../../src/presentation/controllers/organization'
-import { badRequest, serverError } from '../../../../src/presentation/helpers/http-helper'
+import { badRequest, noContent, serverError } from '../../../../src/presentation/helpers/http-helper'
 import { HttpRequest, Validation } from '../../../../src/presentation/protocols'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -79,5 +79,11 @@ describe('AddOrganization Controller', () => {
     })
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
