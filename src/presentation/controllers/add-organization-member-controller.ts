2275fd776b5c795bc3c9b8504eb5f6f15b7b75df
William Simionatto <william.sn0226@gmail.com>
2022-11-26 18:11:32 -0300

    feat: ensure AddOrganizationMemberController calls AddOrganizationMember with correct values

--- src/presentation/controllers/add-organization-member-controller.ts
+++ src/presentation/controllers/add-organization-member-controller.ts
@@ -1,14 +1,21 @@
+import { AddOrganizationMembers } from '../../domain/usecases'
 import { badRequest } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class AddOrganizationMemberController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addOrganizationMembers: AddOrganizationMembers
+  ) {}
 
   async handle (request: AddOrganizationMemberController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+
+    const { organizationId, accountId } = request
+    await this.addOrganizationMembers.add({ organizationId, accountId })
 
     return new Promise(resolve => resolve(null))
   }
@@ -17,6 +24,6 @@ export class AddOrganizationMemberController implements Controller {
 export namespace AddOrganizationMemberController {
   export type Request = {
     organizationId: string
-    memberId: string
+    accountId: string
   }
 }
