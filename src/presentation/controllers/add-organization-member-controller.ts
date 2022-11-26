f7dc07055669b6829497c3c55fad511c80616654
William Simionatto <william.sn0226@gmail.com>
2022-11-26 18:06:31 -0300

    feat: ensure AddOrganizationMemberController returns 400 if Validation returns an error

--- src/presentation/controllers/add-organization-member-controller.ts
+++ src/presentation/controllers/add-organization-member-controller.ts
@@ -1,10 +1,15 @@
+import { badRequest } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class AddOrganizationMemberController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (request: AddOrganizationMemberController.Request): Promise<HttpResponse> {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error) {
+      return badRequest(error)
+    }
+
     return new Promise(resolve => resolve(null))
   }
 }
