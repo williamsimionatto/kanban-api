1cac6312a0c4f9443a5d69bfaf67f30eea49b53e
William Simionatto <william.sn0226@gmail.com>
2022-11-26 18:06:52 -0300

    test: ensure AddOrganizationMemberController returns 400 if Validation returns an error

--- tests/presentation/controllers/add-organization-member.spec.ts
+++ tests/presentation/controllers/add-organization-member.spec.ts
@@ -1,6 +1,7 @@
 import faker from 'faker'
 
 import { AddOrganizationMemberController } from '../../../src/presentation/controllers/add-organization-member-controller'
+import { badRequest } from '../../../src/presentation/helpers'
 import { Validation } from '../../../src/presentation/protocols'
 
 const makeFakeRequest = (): AddOrganizationMemberController.Request => ({
@@ -38,5 +39,12 @@ describe('AddOrganizationMember Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
