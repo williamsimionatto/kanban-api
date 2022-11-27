5839a67d21894d4778fefbede187302888499ec9
William Simionatto <william.sn0226@gmail.com>
2022-11-27 13:59:13 -0300

    feat: ensure DbCheckSurveyById returns false if CheckProjectByIdRepository return false

--- src/data/usecases/db-check-project-by-id.ts
+++ src/data/usecases/db-check-project-by-id.ts
@@ -5,8 +5,6 @@ export class DbCheckProjectById implements CheckProjectById {
   constructor (private readonly checkProjectByIdRepository: CheckProjectByIdRepository) {}
 
   async checkById (id: string): Promise<CheckProjectById.Result> {
-    await this.checkProjectByIdRepository.checkById(id)
-
-    return null
+    return await this.checkProjectByIdRepository.checkById(id)
   }
 }
