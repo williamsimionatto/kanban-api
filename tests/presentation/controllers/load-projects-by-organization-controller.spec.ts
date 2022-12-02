88592909cd14e497bf1af5e39fd7f5857abbbdcc
William Simionatto <william.sn0226@gmail.com>
2022-12-02 19:15:51 -0300

    test: ensure LoadProjectsByOrganizationController returns 200 with projects on success

--- tests/presentation/controllers/load-projects-by-organization-controller.spec.ts
+++ tests/presentation/controllers/load-projects-by-organization-controller.spec.ts
@@ -1,6 +1,7 @@
 import { LoadProjectsByOrganizationController } from '../../../src/presentation/controllers/'
 import faker from 'faker'
 import { LoadProjectsByOrganizationSpy } from '../mocks'
+import { ok } from '../../../src/presentation/helpers'
 
 const mockRequest = (): LoadProjectsByOrganizationController.Request => ({
   organizationId: faker.datatype.uuid()
@@ -27,5 +28,11 @@ describe('LoadProjectsByOrganization Controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(loadProjectsByOrganizationSpy.organizationId).toBe(request.organizationId)
+  })
+
+  test('Should return 200 with projects on success', async () => {
+    const { sut, loadProjectsByOrganizationSpy } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(loadProjectsByOrganizationSpy.result))
   })
 })
