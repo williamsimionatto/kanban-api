1b44bd7ce2b226c538c0b3d6f07d2c803285ccf8
William Simionatto <william.sn0226@gmail.com>
2022-12-02 19:25:19 -0300

    feat: ensure LoadProjectsByOrganizationController return 500 if LoadProjectsByOrganization throws

--- src/presentation/controllers/load-projects-by-organization-controller.ts
+++ src/presentation/controllers/load-projects-by-organization-controller.ts
@@ -1,13 +1,17 @@
 import { LoadProjectsByOrganization } from '../../domain/usecases'
-import { noContent, ok } from '../helpers'
+import { noContent, ok, serverError } from '../helpers'
 import { Controller, HttpResponse } from '../protocols'
 
 export class LoadProjectsByOrganizationController implements Controller {
   constructor (private readonly loadProjectsByOrganization: LoadProjectsByOrganization) {}
 
   async handle (request: LoadProjectsByOrganizationController.Request): Promise<HttpResponse> {
-    const projects = await this.loadProjectsByOrganization.loadByOrganization(request.organizationId)
-    return projects.length ? ok(projects) : noContent()
+    try {
+      const projects = await this.loadProjectsByOrganization.loadByOrganization(request.organizationId)
+      return projects.length ? ok(projects) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
