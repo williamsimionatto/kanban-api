739164ebccdb986aadd3a7f7fe26a53750aa9843
williamsimionatto <william.sn0226@gmail.com>
2023-01-17 21:39:19 -0300

    test: ensure LoadProjectByIdController returns 204 if LoadProjectById returns null

--- tests/presentation/controllers/load-project-by-id-controller.spec.ts
+++ tests/presentation/controllers/load-project-by-id-controller.spec.ts
@@ -1,7 +1,7 @@
 import faker from 'faker'
 
 import { LoadProjectByIdController } from '../../../src/presentation/controllers'
-import { ok } from '../../../src/presentation/helpers'
+import { noContent, ok } from '../../../src/presentation/helpers'
 
 import { LoadProjectByIdSpy } from '../mocks'
 
@@ -36,5 +36,12 @@ describe('LoadProjectByIdController', () => {
     const { sut, loadProjectByIdSpy } = makeSut()
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(ok(loadProjectByIdSpy.result))
+  })
+
+  test('Should return 204 if LoadProjectById returns null', async () => {
+    const { sut, loadProjectByIdSpy } = makeSut()
+    loadProjectByIdSpy.result = null
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
