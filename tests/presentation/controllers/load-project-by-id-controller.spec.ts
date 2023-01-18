22245c30ba8f5d24944fdcff09b9c9d3e630d5b6
williamsimionatto <william.sn0226@gmail.com>
2023-01-17 21:30:06 -0300

    test: ensure LoadProjectByIdController returns 200 with project on success

--- tests/presentation/controllers/load-project-by-id-controller.spec.ts
+++ tests/presentation/controllers/load-project-by-id-controller.spec.ts
@@ -1,6 +1,7 @@
 import faker from 'faker'
 
 import { LoadProjectByIdController } from '../../../src/presentation/controllers'
+import { ok } from '../../../src/presentation/helpers'
 
 import { LoadProjectByIdSpy } from '../mocks'
 
@@ -29,5 +30,11 @@ describe('LoadProjectByIdController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(loadProjectByIdSpy.id).toBe(request.projectId)
+  })
+
+  test('Should return 200 with project on success', async () => {
+    const { sut, loadProjectByIdSpy } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(loadProjectByIdSpy.result))
   })
 })
