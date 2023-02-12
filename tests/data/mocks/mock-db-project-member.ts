32262e9dd81aa9a9ed6c71b1f011c508ec373de2
William Simionatto <william.sn0226@gmail.com>
2023-02-12 10:40:43 -0300

    test: add tests to InactivateProjectMember usecase

--- tests/data/mocks/mock-db-project-member.ts
+++ tests/data/mocks/mock-db-project-member.ts
@@ -1,10 +1,19 @@
-import { ActivateProjectMemberRepository } from '../../../src/data/protocols/db/project'
+import { ActivateProjectMemberRepository, InactivateProjectMemberRepository } from '../../../src/data/protocols/db/project'
 
 export class ActivateProjectMemberRepositorySpy implements ActivateProjectMemberRepository {
   params: ActivateProjectMemberRepository.Params
   result = true
 
   async activate (params: ActivateProjectMemberRepository.Params): Promise<void> {
+    this.params = params
+  }
+}
+
+export class InactivateProjectMemberRepositorySpy implements InactivateProjectMemberRepository {
+  params: InactivateProjectMemberRepository.Params
+  result = true
+
+  async inactivate (params: InactivateProjectMemberRepository.Params): Promise<void> {
     this.params = params
   }
 }
