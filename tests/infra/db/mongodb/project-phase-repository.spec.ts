133e9f204732f23e2751e5d8e403fbe75424c13e
William Simionatto <william.sn0226@gmail.com>
2023-02-15 21:46:24 -0300

    test: ensure ProjectPhaseMongoRepository create an phase for project on add with success

---
+++ tests/infra/db/mongodb/project-phase-repository.spec.ts
@@ -0,0 +1,66 @@
+import FakeObjectId from 'bson-objectid'
+import faker from 'faker'
+import MockDate from 'mockdate'
+import { Collection } from 'mongodb'
+
+import { AddProject, AddProjectPhase } from '../../../../src/domain/usecases'
+import { MongoHelper, ProjectPhaseMongoRepository } from '../../../../src/infra/db/mongodb'
+
+let projects: Collection
+let projectPhase: Collection
+
+const makeProjectParams = (): AddProject.Params => ({
+  name: faker.name.firstName(),
+  description: faker.random.words(),
+  status: faker.random.arrayElement(['active', 'inactive']),
+  startDate: faker.date.recent(),
+  endDate: faker.date.future(),
+  organizationId: new FakeObjectId().toHexString()
+})
+
+const makePhaseParams = (): AddProjectPhase.Params => ({
+  projectId: '',
+  name: faker.name.firstName(),
+  description: faker.random.words(),
+  order: faker.datatype.number(),
+  type: faker.random.arrayElement(['BACKLOG' , 'TODO' , 'DOING' , 'BLOCKED' , 'REVIEW' , 'DONE'])
+})
+
+const makeSut = (): ProjectPhaseMongoRepository => {
+  return new ProjectPhaseMongoRepository()
+}
+
+describe('ProjectPhaseMongoRepository', () => {
+  beforeAll(async () => {
+    MockDate.set(new Date())
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    MockDate.reset()
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    projects = await MongoHelper.getCollection('projects')
+    await projects.deleteMany({})
+
+    projectPhase = await MongoHelper.getCollection('project_phases')
+    await projectPhase.deleteMany({})
+  })
+
+  describe('add()', () => {
+    test('Should create an phase for project on add with success', async () => {
+      const sut = makeSut()
+      const projectParams = makeProjectParams()
+      const res = await projects.insertOne(projectParams)
+      const phaseParams = makePhaseParams()
+      phaseParams.projectId = res.insertedId.toHexString()
+      await sut.add(phaseParams)
+      const projectPhaseData = await projectPhase.find({ name: phaseParams.name }).toArray()
+
+      expect(projectPhaseData).toBeTruthy()
+      expect(projectPhaseData[0].name).toBe(phaseParams.name)
+    })
+  })
+})
