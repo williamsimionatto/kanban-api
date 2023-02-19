406f5ca182d343200d691df08d00cc159769193a
William Simionatto <william.sn0226@gmail.com>
2023-02-19 16:44:11 -0300

    feat: ensure ProjectPhaseMongoRepository returns true if phase exists

--- src/infra/db/mongodb/project-phase-repository.ts
+++ src/infra/db/mongodb/project-phase-repository.ts
@@ -1,13 +1,25 @@
 import { ObjectId } from 'mongodb'
-import { AddProjectPhaseRepository } from '../../../data/protocols/db/project'
+import { AddProjectPhaseRepository, CheckProjectPhaseRepository } from '../../../data/protocols/db/project'
 import { MongoHelper } from './mongo-helper'
 
-export class ProjectPhaseMongoRepository implements AddProjectPhaseRepository {
+export class ProjectPhaseMongoRepository implements
+  AddProjectPhaseRepository,
+  CheckProjectPhaseRepository {
   async add (params: AddProjectPhaseRepository.Params): Promise<void> {
     const projectCollection = await MongoHelper.getCollection('project_phases')
     await projectCollection.insertOne({
       ...params,
       projectId: new ObjectId(params.projectId)
     })
+  }
+
+  async checkPhase (params: CheckProjectPhaseRepository.Params): Promise<CheckProjectPhaseRepository.Result> {
+    const projectCollection = await MongoHelper.getCollection('project_phases')
+    const projectPhase = await projectCollection.findOne({
+      projectId: new ObjectId(params.projectId),
+      type: params.phaseType
+    })
+
+    return projectPhase !== null
   }
 }
