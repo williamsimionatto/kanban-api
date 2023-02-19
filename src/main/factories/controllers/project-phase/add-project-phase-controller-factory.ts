e2581a91e200e83b333ca271441b037e175577a2
William Simionatto <william.sn0226@gmail.com>
2023-02-19 17:38:01 -0300

    feat: inject CheckProjectById dependencie in the AddProjectPhaseController

--- src/main/factories/controllers/project-phase/add-project-phase-controller-factory.ts
+++ src/main/factories/controllers/project-phase/add-project-phase-controller-factory.ts
@@ -1,13 +1,15 @@
 import { AddProjectPhaseController } from '../../../../presentation/controllers'
 import { Controller } from '../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../decorators'
+import { makeDbCheckProjectById } from '../../usecases'
 import { makeDbAddProjectPhase } from '../../usecases/add-project-phase'
 import { makeAddProjectPhaseValidation } from './add-project-phase-validation-factory'
 
 export const makeAddProjectPhaseController = (): Controller => {
   const controller = new AddProjectPhaseController(
     makeAddProjectPhaseValidation(),
-    makeDbAddProjectPhase()
+    makeDbAddProjectPhase(),
+    makeDbCheckProjectById()
   )
 
   return makeLogControllerDecorator(controller)
--- src/presentation/controllers/add-project-phase-controller.ts
+++ src/presentation/controllers/add-project-phase-controller.ts
@@ -1,18 +1,25 @@
-import { AddProjectPhase } from '../../domain/usecases'
-import { badRequest, created, serverError } from '../helpers'
-import { Controller, Validation } from '../protocols'
+import { AddProjectPhase, CheckProjectById } from '../../domain/usecases'
+import { InvalidParamError } from '../errors'
+import { badRequest, created, forbidden, serverError } from '../helpers'
+import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class AddProjectPhaseController implements Controller {
   constructor (
     private readonly validation: Validation,
-    private readonly addProjectPhase: AddProjectPhase
+    private readonly addProjectPhase: AddProjectPhase,
+    private readonly checkProjectById: CheckProjectById
   ) {}
 
-  async handle (request: AddProjectPhaseController.Request): Promise<any> {
+  async handle (request: AddProjectPhaseController.Request): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(request)
       if (error) {
         return badRequest(error)
+      }
+
+      const projectExists = await this.checkProjectById.checkById(request.projectId)
+      if (!projectExists) {
+        return forbidden(new InvalidParamError('projectId'))
       }
 
       await this.addProjectPhase.add(request)
