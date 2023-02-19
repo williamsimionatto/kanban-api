ac99d7534e8ed98cd55f8f85c3b55949460128c5
William Simionatto <william.sn0226@gmail.com>
2023-02-18 21:28:19 -0300

    test: ensure DbCheckProjectPhase call CheckProjectPhaseRepository with correct values

--- tests/data/mocks/mock-db-project-phase.ts
+++ tests/data/mocks/mock-db-project-phase.ts
@@ -1,4 +1,4 @@
-import { AddProjectPhaseRepository } from '../../../src/data/protocols/db/project'
+import { AddProjectPhaseRepository, CheckProjectPhaseRepository } from '../../../src/data/protocols/db/project'
 
 export class AddProjectPhaseRepositorySpy implements AddProjectPhaseRepository {
   params: AddProjectPhaseRepository.Params
@@ -6,5 +6,15 @@ export class AddProjectPhaseRepositorySpy implements AddProjectPhaseRepository {
 
   async add (params: AddProjectPhaseRepository.Params): Promise<void> {
     this.params = params
+  }
+}
+
+export class CheckProjectPhaseRepositorySpy implements CheckProjectPhaseRepository {
+  params: CheckProjectPhaseRepository.Params
+  result = true
+
+  async checkPhase (params: CheckProjectPhaseRepository.Params): Promise<CheckProjectPhaseRepository.Result> {
+    this.params = params
+    return this.result
   }
 }
---
+++ tests/data/usecases/db-check-project-phase.spec.ts
@@ -0,0 +1,35 @@
+import faker from 'faker'
+import { DbCheckProjectPhase } from '../../../src/data/usecases/db-check-project-phase'
+import { PhaseType } from '../../../src/domain/usecases'
+import { CheckProjectPhaseRepositorySpy } from '../mocks'
+
+let projectId: string
+let phaseType: PhaseType
+
+type SutTypes = {
+  sut: DbCheckProjectPhase
+  checkProjectPhaseRepositorySpy: CheckProjectPhaseRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const checkProjectPhaseRepositorySpy = new CheckProjectPhaseRepositorySpy()
+  const sut = new DbCheckProjectPhase(checkProjectPhaseRepositorySpy)
+
+  return {
+    sut,
+    checkProjectPhaseRepositorySpy
+  }
+}
+
+describe('DbCheckProjectPhase', () => {
+  beforeAll(async () => {
+    projectId = faker.datatype.uuid()
+    phaseType = faker.random.arrayElement(['BACKLOG', 'TODO', 'DOING', 'BLOCKED', 'REVIEW', 'DONE'])
+  })
+
+  test('Should call CheckProjectPhaseRepository with correct values', async () => {
+    const { sut, checkProjectPhaseRepositorySpy } = makeSut()
+    await sut.check({ projectId, phaseType })
+    expect(checkProjectPhaseRepositorySpy.params).toEqual({ projectId, phaseType })
+  })
+})
